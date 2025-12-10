466353f1fd22df895034a0637f2d8fa5f4212f4a
Delphine FUMEX <delphinefumex@gmail.com>
2025-12-10 16:16:21 +0100

    Feat: initialisation des pages public

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,24 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/gallery', name: 'app_gallery')]
+    public function renderGallery(): Response
+    {
+        return $this->render('home/gallery.html.twig');
+    }
+
+    #[Route('/services', name: 'app_services')]
+    public function renderServices(): Response
+    {
+        return $this->render('home/services.html.twig');
+    }
+
+    #[Route('/search', name: 'app_search')]
+    public function renderSearch(): Response
+    {
+        return $this->render('home/search.html.twig');
     }
 }
