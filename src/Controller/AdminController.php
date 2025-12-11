65ace8f5b48fde2fa5e2aafe88a5144bc57bf121
Delphine FUMEX <delphinefumex@gmail.com>
2025-12-11 10:18:30 +0100

    Feat: initialisation du contrôleur

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Enum\UserRole;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use App\Repository\RoomRepository;
+use App\Repository\PeriodRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin')]
+final class AdminController extends AbstractController
+{
+    #[Route(name: 'app_settings_index', methods: ['GET'])]
+    public function index(
+        PeriodRepository $periodRepository,
+        RoomRepository $roomRepository,
+        UserRepository $userRepository
+    ): Response {
+
+        // Récupération des périodes
+        $periods = $periodRepository->findActivePeriod();
+
+        // Récupération des chambres
+        $rooms = $roomRepository->findAllRoom();
+
+        // Récupération des utilisateurs
+        $users = $userRepository->findUsersWithRole(UserRole::EMPLOYE->value);
+
+        return $this->render('admin/settings.html.twig', [
+            'periods' => $periods,
+            'rooms' => $rooms,
+            'users' => $users,
+        ]);
+    }
+
+
+
+    //---------------------------------------
+    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
+    public function show(User $user): Response
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    //----------------------------------------------
+
+    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('user/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
