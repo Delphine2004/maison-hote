20d285921764097b98f61a3a8d7634ae25b984c3
Delphine FUMEX <delphinefumex@gmail.com>
2025-12-16 10:48:47 +0100

    Fix: correction sécurité, image et mep

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -5,23 +5,19 @@ namespace App\Controller;
 use App\Entity\Room;
 use App\Enum\RoomStatus;
 use App\Form\RoomType;
-use App\Repository\RoomRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/room')]
+#[IsGranted('ROLE_ADMIN')]
 final class RoomController extends AbstractController
 {
-    #[Route('', name: 'app_room_index', methods: ['GET'])]
-    public function index(RoomRepository $roomRepository): Response
-    {
-        return $this->render('room/index.html.twig', [
-            'rooms' => $roomRepository->findAll(),
-        ]);
-    }
+
+    public function __construct(private string $uploadsRoomsDirectory) {}
 
     #[Route('/new', name: 'app_room_new', methods: ['GET', 'POST'])]
     public function new(
@@ -30,17 +26,18 @@ final class RoomController extends AbstractController
     ): Response {
         $room = new Room();
         $room->setStatus(RoomStatus::AVAILABLE);
-        $form = $this->createForm(RoomType::class, $room);
+        $form = $this->createForm(RoomType::class, $room, ['mode' => 'create']);
         $form->handleRequest($request);
-
-        if ($form->isSubmitted() && !$form->isValid()) {
-            foreach ($form->getErrors(true) as $error) {
-                dump($error->getOrigin()->getName(), $error->getMessage());
-            }
-        }
 
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $uploadedFile = $form->get('picture')->getData();
+            if ($uploadedFile) {
+                $fileName = uniqid() . '.' . $uploadedFile->guessExtension();
+                $uploadedFile->move($this->uploadsRoomsDirectory, $fileName);
+                $room->setPicture($fileName);
+            }
+
             $room->setCreatedBy($this->getUser());
 
             $entityManager->persist($room);
@@ -65,18 +62,36 @@ final class RoomController extends AbstractController
     #[Route('/{id}/edit', name: 'app_room_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Room $room, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(RoomType::class, $room);
+        $oldPicture = $room->getPicture();
+
+        $form = $this->createForm(RoomType::class, $room, ['mode' => 'update']);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $uploadedFile = $form->get('picture')->getData();
+
+            if ($uploadedFile) {
+                $fileName = uniqid() . '.' . $uploadedFile->guessExtension();
+                $uploadedFile->move($this->uploadsRoomsDirectory, $fileName);
+                $room->setPicture($fileName);
+
+                // Suppression de l'ancienne image
+                if ($oldPicture) {
+                    $oldPath = $this->uploadsRoomsDirectory . '/' . $oldPicture;
+                    if (file_exists($oldPath)) {
+                        unlink($oldPath);
+                    }
+                }
+            }
+
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_room_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_settings_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('room/edit.html.twig', [
             'room' => $room,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 
@@ -85,9 +100,16 @@ final class RoomController extends AbstractController
     {
         if ($this->isCsrfTokenValid('delete' . $room->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($room);
+
+            // Suppression du fichier
+            $filePath = $this->uploadsRoomsDirectory . '/' . $room->getPicture();
+            if ($room->getPicture() && file_exists($filePath)) {
+                unlink($filePath);
+            }
+
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_room_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_settings_index', [], Response::HTTP_SEE_OTHER);
     }
 }
