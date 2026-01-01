d6f86f71d6e62f8c18b9d954427ef84587466ff4
Delphine FUMEX <delphinefumex@gmail.com>
2026-01-01 09:52:09 +0100

    Feat: ajout de roomController

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,7 +1,9 @@
 import { renderClientResult } from "./Controller/ClientController.js";
 import { renderBookingResult } from "./Controller/BookingController.js";
+import { renderRoomResult } from "./Controller/RoomController.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     renderClientResult();
     renderBookingResult();
+    renderRoomResult();
 });
