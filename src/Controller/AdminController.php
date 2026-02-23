ea6824794fdb4ebb22f8a003ccaa52448b417bbc
Delphine FUMEX <delphinefumex@gmail.com>
2026-02-23 15:18:39 +0100

    Fix: correction et séparation routes

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -21,29 +21,30 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 final class AdminController extends AbstractController
 {
     #[Route('', name: 'app_settings_index', methods: ['GET'])]
-    public function index(
-        PeriodRepository $periodRepository,
-        RoomRepository $roomRepository,
-        UserRepository $userRepository
-    ): Response {
-
-        // Récupération des périodes
-        $periods = $periodRepository->findActivePeriod();
-
-        // Récupération des chambres
-        $rooms = $roomRepository->findAllRoom();
-
-        // Récupération des utilisateurs
-        $users = $userRepository->findUsersWithoutRoles([UserRole::ADMIN->value, UserRole::CLIENT->value]);
-
-        return $this->render('admin/settings.html.twig', [
-            'periods' => $periods,
-            'rooms' => $rooms,
-            'users' => $users,
-        ]);
+    public function index(): Response
+    {
+        return $this->render('admin/settings_general.html.twig', []);
     }
 
-    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+    #[Route('/hotel', name: 'app_hotel_settings', methods: ['GET'])]
+    public function renderHotelInfos(
+        RoomRepository $roomRepository
+    ): Response {
+        // Récupération des chambres
+        $rooms = $roomRepository->findAllRoom();
+        return $this->render('admin/settings_hotel.html.twig', ['rooms' => $rooms,]);
+    }
+
+    #[Route('/staff', name: 'app_staff_index', methods: ['GET'])]
+    public function renderStaff(UserRepository $userRepository): Response
+    {
+        // Récupération des utilisateurs
+        $users = $userRepository->findUsersWithoutRoles([UserRole::ADMIN->value, UserRole::CLIENT->value]);
+        return $this->render('admin/settings_staff.html.twig', ['users' => $users]);
+    }
+
+
+    #[Route('/staff/new', name: 'app_staff_new', methods: ['GET', 'POST'])]
     public function new(
         Request $request,
         EntityManagerInterface $entityManager,
@@ -51,7 +52,7 @@ final class AdminController extends AbstractController
     ): Response {
         $user = new User();
         $user->setRoles([UserRole::EMPLOYE]);
-        $form = $this->createForm(UserType::class, $user, ['mode' => 'create']);
+        $form = $this->createForm(UserType::class, $user, ['mode' => 'createUser']);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -63,7 +64,7 @@ final class AdminController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_settings_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_staff_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('user/new.html.twig', [
@@ -71,7 +72,31 @@ final class AdminController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
+    #[Route('/{id}/edit', name: 'app_user_edit_by_admin', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->getUser() === $user) {
+            $mode = 'updateAdmin';
+        } else {
+            $mode = 'updateUserByAdmin';
+        }
+
+        $form = $this->createForm(UserType::class, $user, ['mode' => $mode]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user_dashboard', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/staff/{id}', name: 'app_user_delete', methods: ['POST'])]
     public function delete(
         Request $request,
         User $user,
@@ -82,6 +107,6 @@ final class AdminController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_staff_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,6 +7,7 @@ use App\Enum\BookingStatus;
 use App\Enum\UserRole;
 use App\Form\UserType;
 use App\Repository\BookingRepository;
+use App\Repository\UserRepository;
 
 use DateTimeImmutable;
 
@@ -15,14 +16,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 //#[IsGranted(UserRole::EMPLOYE->value)]
 #[Route('/user')]
 final class UserController extends AbstractController
 {
-    #[Route('', name: 'app_user_index', methods: ['GET'])]
+    #[Route('/search', name: 'app_user_index', methods: ['GET'])]
     public function index(
+        UserRepository $userRepository
+    ): Response {
+        return $this->render('user/index.html.twig', [
+            'users' => $userRepository->findUserByFilters([]),
+        ]);
+    }
+
+    #[Route('/dashboard', name: 'app_user_dashboard', methods: ['GET'])]
+    public function renderUserDashboard(
         BookingRepository $bookingRepository
     ): Response {
 
@@ -37,7 +48,7 @@ final class UserController extends AbstractController
         // Récupération des arrivées
         $checkIn = $bookingRepository->findCheckInsForDay($today);
 
-        return $this->render('user/index.html.twig', [
+        return $this->render('booking/index.html.twig', [
             'inhouses' => $inHouse,
             'checkouts' => $checkOut,
             'checkins' => $checkIn,
@@ -45,25 +56,63 @@ final class UserController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
-    public function show(User $user): Response
+    public function showUser(User $user): Response
     {
-        return $this->render('user/show.html.twig', [
+        return $this->render('user/show_user.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/client/{id}', name: 'app_client_show', methods: ['GET'])]
+    public function showClient(User $user): Response
+    {
+        return $this->render('user/show_client.html.twig', [
             'user' => $user,
         ]);
     }
 
     //------------------------------
+    #[Route('/new', name: 'app_client_new', methods: ['GET', 'POST'])]
+    public function new(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response {
+        $user = new User();
+        $user->setRoles([UserRole::CLIENT]);
+        $form = $this->createForm(UserType::class, $user, ['mode' => 'createClient']);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $plainPassword = $form->get('password')->getData();
+            $hashedPassword = $passwordHasher->hashPassword($user, $plainPassword);
+
+            $user->setPassword($hashedPassword);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user_show', ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('user/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
-    {
+    public function edit(
+        Request $request,
+        User $user,
+        EntityManagerInterface $entityManager
+    ): Response {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_user_show', ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('user/edit.html.twig', [
