aac05a0f86c041fdd6d2c81a5e79df620ed4fd2f
Delphine FUMEX <delphinefumex@gmail.com>
2026-02-24 22:08:33 +0100

    Nettoyage

--- public/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-import { renderClientResult } from "./Controller/ClientController.js";
-import { renderBookingResult } from "./Controller/BookingController.js";
-import { renderRoomResult } from "./Controller/RoomController.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-    renderClientResult();
-    renderBookingResult();
-    renderRoomResult();
-});
