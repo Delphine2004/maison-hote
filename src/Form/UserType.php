685a7808385ff9e0b90c36cb14c83b56b1cf73f1
Delphine FUMEX <delphinefumex@gmail.com>
2026-03-12 17:11:58 +0100

    Fix: nettoyage

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,7 +3,6 @@
 namespace App\Form;
 
 use App\Entity\User;
-use App\Enum\UserRole;
 use App\Utils\RegexPatterns;
 
 use Symfony\Component\Form\AbstractType;
@@ -12,7 +11,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
