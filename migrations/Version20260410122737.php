17597cabb08ece800868d0809d3e16e81a6d7f6b
Delphine FUMEX <dfumex2004@gmail.com>
2026-04-11 15:38:57 +0200

    Nouvelle migration

--- migrations/Version20260410122737.php
+++ migrations/Version20260410122737.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260314101049 extends AbstractMigration
+final class Version20260410122737 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,23 +21,15 @@ final class Version20260314101049 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE booking (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, room_id INT DEFAULT NULL, created_by_id INT DEFAULT NULL, updated_by_id INT DEFAULT NULL, starting_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', ending_date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', total_amount_cents INT NOT NULL, status VARCHAR(50) NOT NULL, created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_E00CEDDEA76ED395 (user_id), INDEX IDX_E00CEDDE54177093 (room_id), INDEX IDX_E00CEDDEB03A8386 (created_by_id), INDEX IDX_E00CEDDE896DBBDE (updated_by_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE period (id INT AUTO_INCREMENT NOT NULL, created_by_id INT DEFAULT NULL, updated_by_id INT DEFAULT NULL, name VARCHAR(50) NOT NULL, starting_date DATE NOT NULL COMMENT \'(DC2Type:date_immutable)\', ending_date DATE NOT NULL COMMENT \'(DC2Type:date_immutable)\', created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', UNIQUE INDEX UNIQ_C5B81ECE5E237E06 (name), INDEX IDX_C5B81ECEB03A8386 (created_by_id), INDEX IDX_C5B81ECE896DBBDE (updated_by_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE picture (id INT AUTO_INCREMENT NOT NULL, picture VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE rate (id INT AUTO_INCREMENT NOT NULL, created_by_id INT DEFAULT NULL, updated_by_id INT DEFAULT NULL, room_id INT DEFAULT NULL, period_id INT DEFAULT NULL, amount_cents INT NOT NULL, created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_DFEC3F39B03A8386 (created_by_id), INDEX IDX_DFEC3F39896DBBDE (updated_by_id), INDEX IDX_DFEC3F3954177093 (room_id), INDEX IDX_DFEC3F39EC8B7ADE (period_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE room (id INT AUTO_INCREMENT NOT NULL, created_by_id INT DEFAULT NULL, updated_by_id INT DEFAULT NULL, number INT NOT NULL, name VARCHAR(50) NOT NULL, description LONGTEXT NOT NULL, picture VARCHAR(255) NOT NULL, status VARCHAR(50) NOT NULL, created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', UNIQUE INDEX UNIQ_729F519B96901F54 (number), UNIQUE INDEX UNIQ_729F519B5E237E06 (name), INDEX IDX_729F519BB03A8386 (created_by_id), INDEX IDX_729F519B896DBBDE (updated_by_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE room (id INT AUTO_INCREMENT NOT NULL, created_by_id INT DEFAULT NULL, updated_by_id INT DEFAULT NULL, number INT NOT NULL, name VARCHAR(50) NOT NULL, rate NUMERIC(5, 2) NOT NULL, capacity INT NOT NULL, description LONGTEXT NOT NULL, picture VARCHAR(255) NOT NULL, status VARCHAR(50) NOT NULL, created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', UNIQUE INDEX UNIQ_729F519B96901F54 (number), UNIQUE INDEX UNIQ_729F519B5E237E06 (name), INDEX IDX_729F519BB03A8386 (created_by_id), INDEX IDX_729F519B896DBBDE (updated_by_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE service (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, price VARCHAR(100) NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(100) DEFAULT NULL, last_name VARCHAR(100) DEFAULT NULL, login VARCHAR(25) DEFAULT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON NOT NULL, phone VARCHAR(20) DEFAULT NULL, address VARCHAR(255) DEFAULT NULL, zip_code VARCHAR(10) DEFAULT NULL, city VARCHAR(100) DEFAULT NULL, created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', UNIQUE INDEX UNIQ_8D93D649AA08CB10 (login), UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDEA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDE54177093 FOREIGN KEY (room_id) REFERENCES room (id)');
         $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDEB03A8386 FOREIGN KEY (created_by_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE booking ADD CONSTRAINT FK_E00CEDDE896DBBDE FOREIGN KEY (updated_by_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE period ADD CONSTRAINT FK_C5B81ECEB03A8386 FOREIGN KEY (created_by_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE period ADD CONSTRAINT FK_C5B81ECE896DBBDE FOREIGN KEY (updated_by_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE rate ADD CONSTRAINT FK_DFEC3F39B03A8386 FOREIGN KEY (created_by_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE rate ADD CONSTRAINT FK_DFEC3F39896DBBDE FOREIGN KEY (updated_by_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE rate ADD CONSTRAINT FK_DFEC3F3954177093 FOREIGN KEY (room_id) REFERENCES room (id)');
-        $this->addSql('ALTER TABLE rate ADD CONSTRAINT FK_DFEC3F39EC8B7ADE FOREIGN KEY (period_id) REFERENCES period (id)');
         $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE room ADD CONSTRAINT FK_729F519BB03A8386 FOREIGN KEY (created_by_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE room ADD CONSTRAINT FK_729F519B896DBBDE FOREIGN KEY (updated_by_id) REFERENCES user (id)');
@@ -50,19 +42,11 @@ final class Version20260314101049 extends AbstractMigration
         $this->addSql('ALTER TABLE booking DROP FOREIGN KEY FK_E00CEDDE54177093');
         $this->addSql('ALTER TABLE booking DROP FOREIGN KEY FK_E00CEDDEB03A8386');
         $this->addSql('ALTER TABLE booking DROP FOREIGN KEY FK_E00CEDDE896DBBDE');
-        $this->addSql('ALTER TABLE period DROP FOREIGN KEY FK_C5B81ECEB03A8386');
-        $this->addSql('ALTER TABLE period DROP FOREIGN KEY FK_C5B81ECE896DBBDE');
-        $this->addSql('ALTER TABLE rate DROP FOREIGN KEY FK_DFEC3F39B03A8386');
-        $this->addSql('ALTER TABLE rate DROP FOREIGN KEY FK_DFEC3F39896DBBDE');
-        $this->addSql('ALTER TABLE rate DROP FOREIGN KEY FK_DFEC3F3954177093');
-        $this->addSql('ALTER TABLE rate DROP FOREIGN KEY FK_DFEC3F39EC8B7ADE');
         $this->addSql('ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395');
         $this->addSql('ALTER TABLE room DROP FOREIGN KEY FK_729F519BB03A8386');
         $this->addSql('ALTER TABLE room DROP FOREIGN KEY FK_729F519B896DBBDE');
         $this->addSql('DROP TABLE booking');
-        $this->addSql('DROP TABLE period');
         $this->addSql('DROP TABLE picture');
-        $this->addSql('DROP TABLE rate');
         $this->addSql('DROP TABLE reset_password_request');
         $this->addSql('DROP TABLE room');
         $this->addSql('DROP TABLE service');
