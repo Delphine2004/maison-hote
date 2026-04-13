f6885ff3fd8ea5b5267f4f5b93b9f1f4f9fde394
Delphine FUMEX <dfumex2004@gmail.com>
2026-04-13 16:44:50 +0200

    Fix: simplification de la fonction de recherche

--- src/Repository/RoomRepository.php
+++ src/Repository/RoomRepository.php
@@ -4,7 +4,6 @@ namespace App\Repository;
 
 use App\Entity\Room;
 use App\Enum\BookingStatus;
-use App\Enum\RoomStatus;
 use DateTimeImmutable;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -29,46 +28,33 @@ class RoomRepository extends ServiceEntityRepository
     public function findRoomsByPeriod(
         DateTimeImmutable $start,
         DateTimeImmutable $end,
-        int $limit = 10,
-        string $orderBy = 'ASC'
     ): array {
 
         $bookingStatus = BookingStatus::CONFIRMED;
-        $roomStatus = RoomStatus::AVAILABLE;
 
-        // r = room - p = period - b = booking - ra = rate
+        // r = room - b = booking
         return $this->createQueryBuilder('r')
             ->select([
                 'r.id AS room_id',
                 'r.number',
                 'r.name',
-                'r.status',
-                'ra.amountCents',
+                'r.rate',
                 'DATE_DIFF(:dateEnd, :dateStart) AS nights',
-                '(DATE_DIFF(:dateEnd, :dateStart) * ra.amountCents) AS total_price'
+                '(DATE_DIFF(:dateEnd, :dateStart) * r.rate) AS total_price'
             ])
             ->leftJoin(
                 'r.bookings',
                 'b',
                 'WITH',
-                'b.startingDate < :dateEnd AND b.endingDate > :dateStart AND b.status = :statusBooking'
+                'b.startingDate < :dateEnd 
+                AND b.endingDate > :dateStart 
+                AND b.status = :statusBooking'
             )
-            ->innerJoin('r.rates', 'ra')
-            ->innerJoin(
-                'ra.period',
-                'p',
-                'WITH',
-                'p.startingDate <= :dateStart AND p.endingDate >= :dateEnd'
-            )
-            ->where('r.status = :statusRoom')
-            ->andWhere('b.id IS NULL')
+            ->where('b.id IS NULL')
             ->setParameter('dateStart', $start)
             ->setParameter('dateEnd', $end)
             ->setParameter('statusBooking', $bookingStatus->value)
-            ->setParameter('statusRoom', $roomStatus->value)
 
-            ->orderBy('r.id', $orderBy)
-            ->setMaxResults($limit)
             ->getQuery()
             ->getResult();
     }
