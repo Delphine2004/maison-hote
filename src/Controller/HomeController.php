dc78d982bcf355730dbc40b338d3a9726d00f620
Delphine FUMEX <dfumex2004@gmail.com>
2026-04-13 16:45:20 +0200

    Feat: ajout de la recherche de chambre

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,12 @@ namespace App\Controller;
 
 use App\Repository\PictureRepository;
 use App\Repository\ServiceRepository;
+use App\Repository\RoomRepository;
+
+use App\Form\SearchType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -32,10 +37,32 @@ final class HomeController extends AbstractController
         return $this->render('home/services.html.twig', ['services' => $services]);
     }
 
-    #[Route('/search', name: 'app_search')]
-    public function renderSearch(): Response
-    {
-        return $this->render('home/search.html.twig');
+    #[Route('/search', name: 'app_search', methods: ['GET', 'POST'])]
+    public function renderSearch(
+        Request $request,
+        RoomRepository $roomRepository
+    ): Response {
+        $form = $this->createForm(SearchType::class);
+        $form->handleRequest($request);
+
+        $rooms = [];
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $startDate = $data->startingDate;
+            $endDate   = $data->endingDate;
+
+            $rooms = $roomRepository->findRoomsByPeriod(
+                $startDate,
+                $endDate
+            );
+        }
+
+        return $this->render('home/search.html.twig', [
+            'form' => $form->createView(),
+            'rooms' => $rooms
+        ]);
     }
 
     #[Route('/faq', name: 'app_faq')]
