f69c05d7afcd22a2f46f1c3f2f66ee334629472e
Delphine FUMEX <dfumex2004@gmail.com>
2026-04-13 16:46:16 +0200

    Feat: ajout du formtype

---
+++ src/Form/SearchType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\DTO\SearchData;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class SearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('startingDate', DateType::class, [
+                'widget' => 'single_text',
+                'input' => 'datetime_immutable',
+                'label' => 'Arrivée',
+                'required' => true,
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('endingDate', DateType::class, [
+                'widget' => 'single_text',
+                'input' => 'datetime_immutable',
+                'label' => 'Départ',
+                'required' => true,
+                'attr' => ['class' => 'form-control'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => SearchData::class,
+            'method' => 'GET',
+        ]);
+    }
+}
