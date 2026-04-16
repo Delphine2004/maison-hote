18dd077d134411dcf4512db6bced44a2074a2ede
Delphine FUMEX <dfumex2004@gmail.com>
2026-04-16 21:49:47 +0200

    fix: correction des champs

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -2,54 +2,32 @@
 
 namespace App\Form;
 
-use App\Entity\Booking;
-use App\Entity\Room;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\MoneyType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Validator\Constraints\IsTrue;
+
 
 class BookingType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('startingDate', DateType::class, [
-                'widget' => 'single_text',
-                'label' => 'Arrivée',
-                'required' => true,
-                'attr' => ['class' => 'form-control'],
-            ])
-            ->add('endingDate', DateType::class, [
-                'widget' => 'single_text',
-                'label' => 'Départ',
-                'required' => true,
-                'attr' => ['class' => 'form-control'],
-            ])
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'label' => 'Client',
-                'choice_label' => 'lastName',
-                'required' => true,
-                'attr' => ['class' => 'form-control', 'data-controller' => 'autocomplete'],
-            ])
-            ->add('room', EntityType::class, [
-                'class' => Room::class,
-                'label' => 'Chambre',
-                'choice_label' => 'name',
-                'required' => true,
-                'attr' => ['class' => 'form-control'],
-            ])
-        ;
+            ->add('bookingConsent', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter le traitement des données.'
+                    ]),
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Booking::class,
+            'data_class' => null,
         ]);
     }
 }
