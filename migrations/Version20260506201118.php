bc9c1c0c423bbef7920d4e640491fb75aca43594
Delphine FUMEX <dfumex2004@gmail.com>
2026-05-06 22:31:03 +0200

    Nouvelle migration

--- migrations/Version20260506201118.php
+++ migrations/Version20260506201118.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260504102235 extends AbstractMigration
+final class Version20260506201118 extends AbstractMigration
 {
     public function getDescription(): string
     {
