8893b907a0ba17cac21d999f94d96a4971c1f099
Delphine FUMEX <dfumex2004@gmail.com>
2026-05-06 16:03:28 +0200

    Fix: suppression fonctionnalités supprimer et ajouter

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -19,48 +19,21 @@ final class RoomController extends AbstractController
 
     public function __construct(private string $uploadsRoomsDirectory) {}
 
-    #[Route('/new', name: 'app_room_new', methods: ['GET', 'POST'])]
-    public function new(
-        Request $request,
-        EntityManagerInterface $entityManager
-    ): Response {
-        $room = new Room();
-        $form = $this->createForm(RoomType::class, $room, ['mode' => 'create']);
-        $form->handleRequest($request);
-
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $uploadedFile = $form->get('picture')->getData();
-            if ($uploadedFile) {
-                $fileName = uniqid() . '.' . $uploadedFile->guessExtension();
-                $uploadedFile->move($this->uploadsRoomsDirectory, $fileName);
-                $room->setPicture($fileName);
-            }
-
-            $room->setCreatedBy($this->getUser());
-
-            $entityManager->persist($room);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_settings_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('room/new.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
-
     #[Route('/{id}', name: 'app_room_show', methods: ['GET'])]
-    public function show(Room $room): Response
-    {
+    public function show(
+        Room $room
+    ): Response {
         return $this->render('room/show.html.twig', [
             'room' => $room,
         ]);
     }
 
     #[Route('/{id}/edit', name: 'app_room_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Room $room, EntityManagerInterface $entityManager): Response
-    {
+    public function edit(
+        Request $request,
+        Room $room,
+        EntityManagerInterface $entityManager
+    ): Response {
         $oldPicture = $room->getPicture();
 
         $form = $this->createForm(RoomType::class, $room, ['mode' => 'update']);
@@ -92,23 +65,5 @@ final class RoomController extends AbstractController
             'room' => $room,
             'form' => $form->createView(),
         ]);
-    }
-
-    #[Route('/{id}', name: 'app_room_delete', methods: ['POST'])]
-    public function delete(Request $request, Room $room, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $room->getId(), $request->getPayload()->getString('_token'))) {
-            $entityManager->remove($room);
-
-            // Suppression du fichier
-            $filePath = $this->uploadsRoomsDirectory . '/' . $room->getPicture();
-            if ($room->getPicture() && file_exists($filePath)) {
-                unlink($filePath);
-            }
-
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_settings_index', [], Response::HTTP_SEE_OTHER);
     }
 }
