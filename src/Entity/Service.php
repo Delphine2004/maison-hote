b429f02c3c4049912f672708e647eb522b2c969f
Delphine FUMEX <dfumex2004@gmail.com>
2026-05-07 17:40:10 +0200

    Feat: ajout validation

--- src/Entity/Service.php
+++ src/Entity/Service.php
@@ -3,6 +3,11 @@
 namespace App\Entity;
 
 use App\Repository\ServiceRepository;
+
+use App\Utils\RegexPatterns;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -14,13 +19,20 @@ class Service
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "Le nom est obligatoire.")]
+    #[Assert\Regex(RegexPatterns::ONLY_TEXTE_REGEX)]
+    #[Assert\Length(min: 2, maxMessage: "Le nom doit contenir au minimum 2 lettres.")]
+    #[Assert\Length(max: 255, maxMessage: "Le nom ne doit pas dépasser 255 lettres.")]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $name = null;
 
     #[ORM\Column(length: 100)]
     private ?string $price = null;
 
-    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "La description est obligatoire.")]
+    #[Assert\Regex(RegexPatterns::FREE_TEXT_REGEX)]
+    #[Assert\Length(max: 255, maxMessage: "La description ne doit pas dépasser 255 caractères.")]
+    #[ORM\Column(type: Types::TEXT)]
     private ?string $description = null;
 
     public function getId(): ?int
