e118c3d928511986c061755a4f5add5d1789a4c4
Delphine FUMEX <dfumex2004@gmail.com>
2026-06-02 15:39:03 +0200

    Fix: nettoyage

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,7 +4,6 @@ namespace App\Repository;
 
 use App\Entity\User;
 use App\DTO\SearchClient;
-use App\Enum\UserRole;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
